2be1ae310555458d01b9fb91b2a5da02c1a7c375
Cczzzz <15641680695@163.com>
2019-05-19 15:06:13 +0800

    创建

---
+++ demo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	_ "fmt"
+)
+
+func main() {
+	var  j int = 1
+	var aaaa = "str"
+	var bbb string;
+	fmt.Println("Hello, World!")
+	fmt.Println( j, aaaa,bbb)
+
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+
+}
